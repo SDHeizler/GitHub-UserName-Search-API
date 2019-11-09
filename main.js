4ffce3c8e559a10cd8efc61fe4c6856bba1456c9
Scott Heizler <scottdheizler91@gmail.com>
2019-11-09 12:43:24 -0500

    Finished Github api username search

---
+++ main.js
@@ -0,0 +1,70 @@
+$(function () {
+    console.log('App Loaded Successfully');
+    getUserName();
+});
+
+// Get the searched for username
+function getUserName() {
+    $('form').on('submit', function (event) {
+        event.preventDefault();
+        let user = $('.js-input').val();
+        requestUser(user);
+        $('.results').empty();
+    })
+}
+// Put username into a get request for GitHub
+function requestUser(user) {
+    let url = `https://api.github.com/users/${user}`
+    const options = { method: 'GET' };
+    fetch(url, options)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(function (responseJson) {
+            getUserRepos(responseJson);
+        })
+        .catch(function (err) {
+            console.log(`There was an error ${err.message}`);
+        })
+}
+// Get user repos
+function getUserRepos(responseJson) {
+    let repo = responseJson.repos_url;
+    const options = { method: 'GET' };
+    fetch(repo, options)
+        .then(function (response) {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(function (responseJson) {
+            displayRepos(responseJson);
+        })
+        .catch(function (err) {
+            console.log(`There was an error ${err.message}`);
+        })
+
+}
+// Display user repos
+function displayRepos(responseJson) {
+    let nameArr = [];
+    let linkArr = [];
+    responseJson.forEach(function (ele) {
+        nameArr.push([ele.name, ele.html_url]);
+        $('.results').append(`<li>
+                    <a href="${ele.html_url}" target="_blank">${ele.name}</a>
+                </li>`);
+    })
+    clearInput();
+}
+
+function clearInput() {
+    $('.js-input').val("");
+}
+
+
+
